5ded326b7001acfe60298d9b79afc6a985e2bc01
chenxin <chenxin411@jd.com>
2021-01-20 20:59:35 +0800

    1

--- application.js
+++ application.js
@@ -167,7 +167,7 @@ System.register([], function (_export, _context) {
     var launchScene = settings.launchScene; // load scene
 
     cc.director.loadScene(launchScene, null, function () {
-      cc.view.setDesignResolutionSize(1080, 1920, 3);
+      cc.view.setDesignResolutionSize(1080, 1920, 4);
       console.log("Success to load scene: ".concat(launchScene));
     });
   }
